f322807f4781f935cf45fb60638d1737f9926003
XavierDupuis <xavier0978@hotmail.fr>
2023-03-22 10:59:14 -0400

    simplify script for parsing

--- cassandra/run.sh
+++ cassandra/run.sh
@@ -3,12 +3,10 @@ file=/mnt/workloads_$(date +%F-%T-%N).txt
 > ${file}
 for i in {a..f}
 do
-    echo - - BEGIN WORLOAD $i - - >> ${file}
+    echo WORKLOAD,$i,f >> ${file}
     for j in $(seq 1 $ITERATIONS)
     do
-        echo - ITERATION $j of $ITERATIONS - >> ${file}
+        echo ITERATION,$j,$ITERATIONS >> ${file}
         bin/ycsb.sh load basic -P workloads/workload$i | grep ^"\[" >> ${file}
     done
-    echo - END WORLOAD $i - >> ${file}
-    echo $'\n' >> ${file}
 done
