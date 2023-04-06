690d96f876752a704e76f029d2db8452b2f5811c
XavierDupuis <xavier0978@hotmail.fr>
2023-04-06 15:34:33 -0400

    adapt run script

--- cassandra/run.sh
+++ cassandra/run.sh
@@ -1,12 +1,41 @@
-#!/bin/sh
-file=/mnt/workloads_$(date +%F-%T-%N).txt
-> ${file}
-for i in {a..f}
+#!/bin/bash
+git clone http://github.com/brianfrankcooper/YCSB.git
+cd YCSB
+mkdir out
+touch out/LoadCassandra.csv
+touch out/RunCassandra.csv
+
+docker exec -it cassandra1 cqlsh 192.168.5.2 -u cassandra -p cassandra -e "create keyspace ycsb WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor':3}"
+
+docker exec -it cassandra1 cqlsh 192.168.5.2 -u cassandra -p cassandra -e "create table ycsb.usertable (
+    y_id varchar primary key,
+    field0 varchar,
+    field1 varchar,
+    field2 varchar,
+    field3 varchar,
+    field4 varchar,
+    field5 varchar,
+    field6 varchar,
+    field7 varchar,
+    field8 varchar,
+    field9 varchar);"
+
+for i in {c a f}
 do
-    echo WORKLOAD,$i,f >> ${file}
-    for j in $(seq 1 $ITERATIONS)
-    do
-        echo ITERATION,$j,$ITERATIONS >> ${file}
-        bin/ycsb.sh load basic -P workloads/workload$i | grep ^"\[" >> ${file}
-    done
+	echo WORKLOAD,$i,3 >> out/LoadCassandra.txt
+	echo WORKLOAD,$i,3 >> out/RunCassandra.txt
+	for j in {1..3}
+	do
+		echo ITERATION,$j,3 >> out/LoadCassandra.txt
+		./bin/ycsb load cassandra-cql -s -P workload$i \
+		-p "hosts=192.168.5.2,192.168.5.3,192.168.5.4,192.168.5.5" \
+		-p "cassandra.password=cassandra" \
+		-p "cassandra.username=cassandra" >> out/LoadCassandra.txt
+
+		echo ITERATION,$j,3 >> out/RunCassandra.txt
+		./bin/ycsb run cassandra-cql -s -P workload$i \
+		-p "hosts=192.168.5.2,192.168.5.3,192.168.5.4,192.168.5.5" \
+		-p "cassandra.password=cassandra" \
+		-p "cassandra.username=cassandra" >> out/RunCassandra.txt
+	done
 done
